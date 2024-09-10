3d4171cb90de66f0fb7d9d94cbc6d4684dfcf993
Scar-o <130754969+Scar-o@users.noreply.github.com>
2024-09-10 15:16:35 +0300

    change made

--- src/oscar.java
+++ src/oscar.java
@@ -1,7 +1,8 @@
 public class oscar {
     public static void main(String[] args) {
         //detta är min egen klass
-
+        String hej = "pog";
+        System.out.println(hej);
         //jag gör nu en change och committar den
 
     }
