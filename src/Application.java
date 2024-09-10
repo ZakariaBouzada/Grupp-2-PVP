d53e11f8d1976ca4437ec92370a9007bbe67c4d6
Niki <niki.zte@gmail.com>
2024-09-10 15:19:23 +0300

    testing round 4

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
     //Hej
     String hej = "Hej";
     System.out.println(hej);
-    System.out.println("test nummer 2");
+    System.out.println("test nummer 5");
 
 
 
