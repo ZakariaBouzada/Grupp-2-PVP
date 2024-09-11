3c49412c5b45a2069aefde02809653f4ed62e515
ZakariaBouzada <106018668+ZakariaBouzada@users.noreply.github.com>
2024-09-11 15:42:29 +0300

    Update Customer.java

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,1 +1,21 @@
+public class Customer {
+    private String id;
+    private String name;
+    private boolean hasBonusCard;
+    private double walletAmount;
+    private int bonusPoints;
 
+    public Customer(String id, String name, boolean hasBonusCard) {
+        this.id = id;
+        this.name = name;
+        this.hasBonusCard = hasBonusCard;
+    }
+
+    public void payWithCreditCard(double amount) {
+        // Logic for credit card payment
+    }
+
+    public void payWithCash(double amount) {
+        // Logic for cash payment
+    }
+}
